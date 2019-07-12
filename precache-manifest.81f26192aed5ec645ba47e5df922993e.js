2a2788377b335a080a0f823566352e16bccee172
Kamila Sieczkowska (KIO) <KIO@kmd.dk>
2019-07-12 15:03:03 +0200

    Updates

---
+++ precache-manifest.81f26192aed5ec645ba47e5df922993e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c05e4247f952d4c3cd99292f80da1997",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5e50b27f44bb17d65ee",
+    "url": "/static/css/main.be461ffa.chunk.css"
+  },
+  {
+    "revision": "400ca711028176baa6ab",
+    "url": "/static/js/2.3d65e42c.chunk.js"
+  },
+  {
+    "revision": "c5e50b27f44bb17d65ee",
+    "url": "/static/js/main.6f143071.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
